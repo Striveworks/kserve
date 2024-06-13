3469fdc0fe7325dae7e8f0427f9c094bea3b9bbc
Scott Pollack <s.pollack@striveworks.com>
2024-06-13 14:57:55 +0000

    timestamp in iso format

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -28,6 +28,7 @@ import (
 	"github.com/kserve/kserve/pkg/controller/v1alpha1/trainedmodel/reconcilers/modelconfig"
 	v1beta1controller "github.com/kserve/kserve/pkg/controller/v1beta1/inferenceservice"
 	"github.com/kserve/kserve/pkg/webhook/admission/pod"
+	"go.uber.org/zap/zapcore"
 	istio_networking "istio.io/api/networking/v1alpha3"
 	"istio.io/client-go/pkg/apis/networking/v1alpha3"
 	v1 "k8s.io/api/core/v1"
@@ -91,7 +92,10 @@ func init() {
 }
 
 func main() {
-	logf.SetLogger(zap.New())
+	logf.SetLogger(zap.New(zap.UseFlagOptions(&zap.Options{
+		TimeEncoder: zapcore.ISO8601TimeEncoder,
+	})))
+
 	log := logf.Log.WithName("entrypoint")
 
 	// Get a config to talk to the apiserver
